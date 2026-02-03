6464981199f7ae1b637682abe8258e5e1b86e6b2
Mikiyas Dawit <educationforlife424@gmail.com>
2026-02-03 15:19:22 +0300

    solve: leetcode longest common prefix simple

---
+++ python_prefix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        
+        m = ""
+        for i in range(len(strs[0])):
+            ch = strs[0][i] 
+            for s in strs[1:]:
+                if i >= len(s) or s[i] != ch:
+                    return m
+            m += ch
+        
+        return m
