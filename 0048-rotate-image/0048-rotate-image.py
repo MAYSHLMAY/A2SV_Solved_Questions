4b83d934998da5089c3709b7b4e89b65565a919f
Mikiyas Dawit <90477617+MAYSHLMAY@users.noreply.github.com>
2026-02-17 01:59:45 +0300

    Time: 0 ms (100%), Space: 19.4 MB (49.44%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,20 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
 
-        layers = len(matrix) // 2
+        n = len(matrix)
 
-        for i in range(layers):
-
-            first = i
-            last = len(matrix) - 1 - i
-
-            for j in range(first, last):
-                offset = j - first
-
-                top = matrix[first][j]
-                matrix[first][j] = matrix[last - offset][first] 
-                matrix[last - offset][first] = matrix[last][last - offset]  
-                matrix[last][last - offset] = matrix[j][last] 
-                matrix[j][last] = top  
+        for i in range(n // 2):
+            for j in range(i, n - 1 - i):
+              (matrix[i][j], 
+                 matrix[j][n-1-i], 
+                 matrix[n-1-i][n-1-j], 
+                 matrix[n-1-j][i]) = (matrix[n-1-j][i], 
+                                      matrix[i][j], 
+                                      matrix[j][n-1-i], 
+                                      matrix[n-1-i][n-1-j])
