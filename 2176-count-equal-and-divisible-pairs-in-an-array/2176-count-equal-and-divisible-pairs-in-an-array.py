01621c177517fb1769a27850a0c8d6b4da5881af
Mikiyas Dawit <90477617+MAYSHLMAY@users.noreply.github.com>
2026-02-19 13:53:57 +0300

    Time: 11 ms (87.58%), Space: 19.4 MB (15.73%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -4,18 +4,20 @@ class Solution:
     def countPairs(self, nums, k):
         count = 0
         freq = defaultdict(list)
-        
+
         for i, v in enumerate(nums):
             freq[v].append(i)
         
         print(freq)
 
         for num, f in freq.items():
+            bucket = [0] * k 
             n = len(f)
-            if n >= 2:
-                for i in range(n):
-                    for j in range(i + 1, n):
-                        if (f[i] * f[j]) % k == 0:
-                            count += 1
+            
+            for i in f:
+                r = i % k
+                for r2 in range(k):
+                    if (r * r2) % k == 0: count += bucket[r2]
+                bucket[r] += 1  
 
         return count
