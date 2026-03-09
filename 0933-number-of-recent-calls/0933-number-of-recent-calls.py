628a7f0af4b521062d259b36a9104d1682207344
Mikiyas Dawit <90477617+MAYSHLMAY@users.noreply.github.com>
2026-03-09 19:36:36 +0300

    Time: 31 ms (82.69%), Space: 24.7 MB (50.86%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -4,8 +4,7 @@ class RecentCounter:
         self.q = []
 
     def ping(self, t: int) -> int:
-        # Remove all requests NOT in the range [t-3000, t]
-        while self.q and not (t - 3000 <= self.q[0] <= t):
+        while self.q and not (t - 3000 <= self.q[0]):
             self.q.pop(0)
         
         self.q.append(t)
